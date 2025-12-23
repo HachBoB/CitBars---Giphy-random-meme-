8e9de17b288775b5abf9930b8a5a1791e5c7286c
Konstantin <57441046+HachBoB@users.noreply.github.com>
2025-12-23 13:09:49 +0400

    fix styles

--- src/features/MemeCard/components/MemeCard.jsx
+++ src/features/MemeCard/components/MemeCard.jsx
@@ -5,7 +5,7 @@ export function MemeCard({ meme }) {
 
     return (
         <article className="w-[30%] mt-[1%] rounded-xl p-4 flex flex-col gap-2
-                   bg-neutral-900/40 border border-white/10
+                   border border-white/10
                    shadow-[0_25px_60px_rgba(0,0,0,0.7)] backdrop-blur-sm">
             <div className=" h-[80%] bg-black overflow-hidden ">
                 <img
@@ -16,7 +16,7 @@ export function MemeCard({ meme }) {
                 />
             </div>
 
-            <div className="text-sm font-semibold line-clamp-2">{meme.title}</div>
+            <div className="text-sm font-semibold">{meme.title}</div>
             <button
                 onClick={handleClick}
                 className="text-xs underline text-white/70 self-start"
